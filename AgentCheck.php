223c6872b78892367dc96ecd558ceb9d5c1f3728
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2026-05-08 23:56:12 +0530

    Initial Commit

---
+++ AgentCheck.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Apps\Tms\Middlewares\AgentCheck;
+
+use System\Base\BaseMiddleware;
+
+class AgentCheck extends BaseMiddleware
+{
+    public function process($data)
+    {
+        //
+    }
+}
